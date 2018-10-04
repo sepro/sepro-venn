eb90b2681dbdeb7b41347192211471f141fbf82d
sepro <sebastian.proost@gmail.com>
2018-10-04 15:02:17 +0200

    added stroke

--- src/components/stencil-venn/stencil-venn.tsx
+++ src/components/stencil-venn/stencil-venn.tsx
@@ -30,12 +30,12 @@ export class StencilVenn {
   render() {
     return <svg id="venn-svg" width={ this.width() } height={ this.height() } version="1.1" xmlns="http://www.w3.org/2000/svg">
     <g transform={ this.transform() }>
-     <g>
+     <g stroke="#fff" stroke-width="0.5">
       <path class="grow" d="m34.019 5.1642a17.236 13.106 0 0 0-6.2043 10.054 17.236 13.106 0 0 0 6.1842 10.055 17.236 13.106 0 0 0 6.2038-10.055 17.236 13.106 0 0 0-6.1837-10.054z" fill="#87de87"/>
       <path class="grow" d="m22.385 1.4226a18.143 13.796 0 0 0-18.143 13.796 18.143 13.796 0 0 0 18.143 13.796 18.143 13.796 0 0 0 11.612-3.2127 18.143 13.796 0 0 1-6.5097-10.583 18.143 13.796 0 0 1 6.5309-10.584 18.143 13.796 0 0 0-11.633-3.2122z" fill="#fd5"/>
       <path class="grow" d="m34.019 4.6348a18.143 13.796 0 0 1 6.5097 10.584 18.143 13.796 0 0 1-6.5309 10.583 18.143 13.796 0 0 0 11.633 3.2127 18.143 13.796 0 0 0 18.143-13.796 18.143 13.796 0 0 0-18.143-13.796 18.143 13.796 0 0 0-11.612 3.2122z" fill="#80b3ff"/>
      </g>
-     <g fill="#000000" font-family="sans-serif" font-size="10.583px" letter-spacing="0px" stroke-width=".26458" word-spacing="0px">
+     <g fill="#000000" font-family="sans-serif" font-size="10.583px" letter-spacing="0px" stroke-width="0" word-spacing="0px">
       <text x="19.130795" y="16.756538" style="line-height:1.25"><tspan x="19.130795" y="16.756538" font-size="4.2333px" stroke-width=".26458" text-align="center" text-anchor="middle">{ this.left_value }</tspan></text>
       <text x="34.008442" y="16.756538" style="line-height:1.25"><tspan x="34.008442" y="16.756538" font-size="4.2333px" stroke-width=".26458" text-align="center" text-anchor="middle">{ this.intersection_value }</tspan></text>
       <text x="48.885826" y="16.756538" style="line-height:1.25"><tspan x="48.885826" y="16.756538" font-size="4.2333px" stroke-width=".26458" text-align="center" text-anchor="middle">{ this.right_value }</tspan></text>
